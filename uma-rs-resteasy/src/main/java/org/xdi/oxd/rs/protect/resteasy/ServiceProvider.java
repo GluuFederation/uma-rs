0fe6e5f822733997b69cab626446d5af2c5d1d7c
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2016-04-26 13:09:27 +0300

    added ability to query AS host from well known url

--- uma-rs-resteasy/src/main/java/org/xdi/oxd/rs/protect/resteasy/ServiceProvider.java
+++ uma-rs-resteasy/src/main/java/org/xdi/oxd/rs/protect/resteasy/ServiceProvider.java
@@ -1,9 +1,14 @@
 package org.xdi.oxd.rs.protect.resteasy;
 
+import org.apache.log4j.Logger;
+import org.xdi.oxauth.client.uma.PermissionRegistrationService;
 import org.xdi.oxauth.client.uma.ResourceSetRegistrationService;
 import org.xdi.oxauth.client.uma.UmaClientFactory;
 import org.xdi.oxauth.client.uma.UmaConfigurationService;
 import org.xdi.oxauth.model.uma.UmaConfiguration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * @author Yuriy Zabrovarnyy
@@ -12,10 +17,13 @@ import org.xdi.oxauth.model.uma.UmaConfiguration;
 
 public class ServiceProvider {
 
+    private static final Logger LOG = Logger.getLogger(ServiceProvider.class);
+
     private final Configuration configuration;
     private UmaConfiguration umaConfiguration = null;
     private UmaConfigurationService configurationService = null;
     private ResourceSetRegistrationService resourceSetRegistrationService = null;
+    private PermissionRegistrationService permissionRegistrationService;
 
     public ServiceProvider(Configuration configuration) {
         this.configuration = configuration;
@@ -44,5 +52,22 @@ public class ServiceProvider {
             resourceSetRegistrationService = UmaClientFactory.instance().createResourceSetRegistrationService(umaConfiguration);
         }
         return resourceSetRegistrationService;
+    }
+
+    public synchronized PermissionRegistrationService getPermissionRegistrationService() {
+        if (permissionRegistrationService == null) {
+            permissionRegistrationService = UmaClientFactory.instance().createResourceSetPermissionRegistrationService(umaConfiguration);
+        }
+        return permissionRegistrationService;
+    }
+
+
+    public String getAmHost() {
+        try {
+            return new URI(getConfiguration().getUmaWellknownEndpoint()).getHost();
+        } catch (URISyntaxException e) {
+            LOG.error("Failed to parse well-known endpoint from configuration: " + getConfiguration().getUmaWellknownEndpoint(),e);
+        }
+        return "";
     }
 }
